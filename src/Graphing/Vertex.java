08c648c8c825ae6590e9c01f49edf4328707e8c0
Phillip Wall <tsyshadow@gmail.com>
2015-11-23 10:00:16 -0500

    Refined Vertex
    - Modified path finding algorithms
      - Now uses helper methods
      - They no longer take arguments and will return a list of edges
      - Will now detect cycles
    - Modified arrival and mustLeave methods to expect the new interface for the path methods

--- src/Graphing/Vertex.java
+++ src/Graphing/Vertex.java
@@ -1,6 +1,8 @@
 package Graphing;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -26,17 +28,29 @@ public class Vertex {
         frontNodes = fV;
     }
 
-    public void getMaxPathToStart(ArrayList<Edge> path) {
+    public List<Edge> getMaxPathToStart() {
+        ArrayList<Edge> ret = new ArrayList<>();
+
+        getMaxPathToStartHelper(ret, new HashSet<Integer>());
+        return ret;
+    }
+
+    private void getMaxPathToStartHelper(ArrayList<Edge> path, HashSet<Integer> visited) {
         if (backNodes.isEmpty()) {return;}
 
         ArrayList<Edge> currentPath = null;
         int currentWeight = 0;
+        visited.add(this.value);
         for (Integer v:backNodes) {
+            if (visited.contains(v)) {
+                throw new CycleException("A cycle was detected when traversing from v" + value + " to v" + v);
+            }
             ArrayList<Edge> newPath = new ArrayList<>();
             Edge e = new Edge(v, this.value, graph.Weight(v, value));
             newPath.add(e);
             Vertex vert = graph.getVertex(v);
-            vert.getMaxPathToStart(newPath);
+            //noinspection unchecked
+            vert.getMaxPathToStartHelper(newPath, (HashSet<Integer>) visited.clone());
             int w = Edge.sumEdges(newPath);
             if (w>currentWeight) {
                 currentWeight = w;
@@ -47,17 +61,29 @@ public class Vertex {
         path.addAll(currentPath);
     }
 
-    public void getMaxPathToFinish(ArrayList<Edge> path) {
+    public List<Edge> getMaxPathToFinish() {
+        ArrayList<Edge> ret = new ArrayList<>();
+        getMaxPathToFinishHelper(ret, new HashSet<Integer>());
+        return ret;
+    }
+
+    private void getMaxPathToFinishHelper(ArrayList<Edge> path, HashSet<Integer> visited) {
         if (frontNodes.isEmpty()) {return;}
 
         ArrayList<Edge> currentPath = null;
         int currentWeight = 0;
+
+        visited.add(value);
         for (Integer v:frontNodes) {
+            if (visited.contains(v)) {
+                throw new CycleException("A cycle was detected when traversing from v" + value + " to v" + v);
+            }
             ArrayList<Edge> newPath = new ArrayList<>();
             Edge e = new Edge(this.value, v, graph.Weight(value, v));
             newPath.add(e);
             Vertex vert = graph.getVertex(v);
-            vert.getMaxPathToFinish(newPath);
+            //noinspection unchecked
+            vert.getMaxPathToFinishHelper(newPath, (HashSet<Integer>) visited.clone());
             int w = Edge.sumEdges(newPath);
             if (w>currentWeight) {
                 currentWeight = w;
@@ -69,15 +95,11 @@ public class Vertex {
     }
 
     public int arrival() {
-        ArrayList<Edge> path = new ArrayList<>();
-        getMaxPathToStart(path);
-        return Edge.sumEdges(path);
+        return Edge.sumEdges(getMaxPathToStart());
     }
 
     public int mustLeave() {
-        ArrayList<Edge> path = new ArrayList<>();
-        getMaxPathToFinish(path);
-        int finish = Edge.sumEdges(path);
+        int finish = Edge.sumEdges(getMaxPathToFinish());
         return graph.minCompletionWeight() - finish;
     }
 
